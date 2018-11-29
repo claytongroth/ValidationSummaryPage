e23c21913ca0a3e9b8699e9c7d6e9171f24cf03f
claytongroth <37966122+claytongroth@users.noreply.github.com>
2018-11-29 13:09:38 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,167 @@
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      validation: []
+    };
+  }
+  componentWillMount(){
+    this.setState({
+      validation: testValues
+    }, () => console.log("State: ", this.state.validation)
+  )
+  }
+   render() {
+     const fd = this.state.validation.Fields_Diffs
+     const inL = this.state.validation.inLineErrors
+     const bLL = this.state.validation.broadLevelErrors
+      return (
+         <div>
+            <h1>Validation Summary:</h1>
+            <InLineErrors inline={inL}/>
+            <BroadLevelErrors broadLevel={bLL} />
+            <Positive positives={fd}/>
+            <Negative negatives={fd}/>
+            <Zero zeroes={fd}/>
+         </div>
+      );
+   }
+}
+class InLineErrors extends React.Component {
+    list(){
+      var p = this.props.inline
+      var listArray = []
+      for (var i in p){
+          listArray.push(<li id={i} key={i}><b>{i + ": "}</b> {+ p[i]}</li>);
+      }
+      return listArray
+    }
+    render() {
+    return (
+     <div id="inLine">
+       <h3> In Line Errors</h3>
+        <p> {this.list()}</p>
+     </div>
+    );
+    }
+}
+class BroadLevelErrors extends React.Component {
+  list(){
+    var p = this.props.broadLevel
+    var listArray = []
+    for (var i in p){
+        listArray.push(<li id={i} key={i}><b>{i + ": "}</b> {+ p[i]}</li>);
+    }
+    return listArray
+  }
+  render() {
+    return (
+       <div id="broadlevel">
+        <h3> Broad Level Errors</h3>
+        <p> {this.list()}</p>
+       </div>
+    );
+  }
+}
+class Zero extends React.Component {
+  list(){
+    var p = this.props.zeroes
+    var listArray = []
+    for (var i in p){
+      if (p[i] == 0){
+        listArray.push(<li id={i} key={i}><a id="desc">{i + ": "}</a> {+ p[i]}</li>);
+      }
+    }
+    return listArray
+  }
+   render() {
+      return (
+         <div id="zeroes">
+          <h1>Zero Values</h1>
+           <ul>
+           {this.list()}
+           </ul>
+         </div>
+      );
+   }
+}
+class Positive extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hover: false,
+      tooltip: ""
+    }
+  }
+  hoverHandler(e){
+    this.setState({
+      hover: true,
+      tooltip: e.target.id
+    })
+    console.log(e.target, "hovering")
+  }
+  leaveHandler(e){
+    this.setState({
+      hover: false,
+      tooltip: ""
+    })
+    console.log(e.target, "leaving")
+  }
+  list(){
+    var p = this.props.positives
+    var listArray = []
+    for (var i in p){
+      if (p[i] > 0){
+        listArray.push(<li key={i}><a id={i} value={p[i]} onMouseEnter={this.hoverHandler.bind(this)} onMouseLeave={this.leaveHandler.bind(this)} id="desc">{i + ": "}</a> {+ p[i]}</li>);
+      }
+    }
+    return listArray.sort(function(a, b){return a.props.value - b.props.value});
+  }
+   render() {
+     const tooltipStyle = {
+        display: this.state.hover ? 'block' : 'none'
+      }
+
+      return (
+         <div id="positives">
+          <h1>Positive Values</h1>
+            <div id="tooltip" style={tooltipStyle}>{this.state.tooltip}</div>
+
+           <ul>
+           {this.list()}
+           </ul>
+         </div>
+      );
+   }
+}
+class Negative extends React.Component {
+  list(){
+    var p = this.props.negatives
+    var listArray = []
+    for (var i in p){
+      if (p[i] < 0){
+        listArray.push(<li id={i} key={i} value={p[i]}><a id="desc">{i + ": "}</a> {+ p[i]}</li>);
+      }
+    }
+    console.log(listArray[0].props.value)
+    return listArray.sort(function(a, b){return a.props.value - b.props.value});
+  }
+   render() {
+      return (
+         <div id="negatives">
+         <h1>Negative Values</h1>
+           <ul>
+           {this.list()}
+           </ul>
+         </div>
+      );
+   }
+}
+
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+// CTRL + SHIFT + H launch
---
+++ testjson.js
@@ -0,0 +1,64 @@
+const testValues = {
+  "Records_Missing": {
+    "Missing_CONAME": "0",
+    "Missing_PARCELSRC": "3",
+    "Missing_PARCELFIPS": "3"
+   },
+   "Fields_Diffs": {
+      "PLACENAME": "-23747",
+      "GISACRES": "-23728",
+      "CONAME": "-23741",
+      "SUFFIX": "-41",
+      "STREETTYPE": "-7661",
+      "TAXPARCELID": "0",
+      "PARCELID": "-23741",
+      "ZIPCODE": "-9364",
+      "PARCELSRC": "-23744",
+      "PARCELFIPS": "-23744",
+      "PREFIX": "-3618",
+      "AUXCLASS": "-3731",
+      "NETPRPTA": "-22981",
+      "SCHOOLDISTNO": "-23747",
+      "UNITID": "-8",
+      "LNDVALUE": "-22984",
+      "OWNERNME1": "-22986",
+      "OWNERNME2": "-3197",
+      "IMPVALUE": "-22986",
+      "CNTASSDVALUE": "-22984",
+      "DEEDACRES": "147",
+      "TAXROLLYEAR": "-23741",
+      "PSTLADRESS": "-22746",
+      "IMPROVED": "-22984",
+      "ESTFMKVALUE": "-22211",
+      "ASSDACRES": "-19357",
+      "FORESTVALUE": "0",
+      "ADDNUMPREFIX": "-5870",
+      "ADDNUMSUFFIX": "-509",
+      "ADDNUM": "-9363",
+      "LANDMARKNAME": "0",
+      "PROPCLASS": "-20194",
+      "UNITTYPE": "-2",
+      "SCHOOLDIST": "-23747",
+      "SITEADRESS": "-9364",
+      "GRSPRPTA": "-22981",
+      "PARCELDATE": "-23121",
+      "STREETNAME": "-9364",
+      "ZIP4": "0"
+    },
+    "Tax_Roll_Years_Pcnt": {
+      "Previous_Taxroll_Year": "0.0",
+      "Expected_Taxroll_Year": "100.0",
+      "Other_Taxroll_Years": "0.0",
+      "Future_Taxroll_Years": "0.0"
+    },
+    "inLineErrors": {
+      "Tax_Errors": "2",
+      "Address_Errors": "4",
+      "General_Errors": "6",
+      "Geometric_Errors": "0"
+    },
+    "broadLevelErrors": {
+      "Geometric_File_Error": ["None"],
+      "Geometric_Misplacement_Flag": ["None"]
+    }
+}
